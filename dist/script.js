36458083835855927a3e9234044c6607cc775295
somnathbedia <somnathbedia7@gmail.com>
2024-01-19 23:48:52 +0530

    graphql-1 test done

--- dist/script.js
+++ dist/script.js
@@ -23,12 +23,67 @@ dotenv_1.default.config();
 const userRoutes = require('./controllers/user-controller');
 const adminRoutes = require('./controllers/admin-controller');
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8000;
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Admin {
+    id: ID
+    username: String
+    email: String
+    password: String
+    course: String
+  }
+
+  type User{
+    id:ID
+    username:String
+    fullname:String
+    email:String
+    password:String
+    course:Course
+  }
+
+  type Course{
+    id:ID
+    title:String
+    description: String
+    adminId:String
+  }
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    getAllAdmin: [Admin]
+    getAllCourses: [Course]
+  }
+`;
+const resolvers = {
+    Query: {
+        getAllAdmin: () => __awaiter(void 0, void 0, void 0, function* () {
+            const admins = yield exports.prisma.admin.findMany();
+            return admins;
+        }),
+        getAllCourses: () => __awaiter(void 0, void 0, void 0, function* () {
+            const courses = yield exports.prisma.course.findMany();
+            return courses;
+        })
+    },
+};
 const app = (0, express_1.default)();
 exports.prisma = new client_1.PrismaClient();
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
+        const server = new server_1.ApolloServer({
+            typeDefs,
+            resolvers,
+        });
         app.use(body_parser_1.default.json());
         app.use((0, cors_1.default)());
+        yield server.start();
+        app.use('/graphql', (0, express4_1.expressMiddleware)(server));
         app.use('/user', userRoutes);
         app.use('/admin', adminRoutes);
         app.get('/', (req, res) => {
--- script.ts
+++ script.ts
@@ -9,14 +9,75 @@ const userRoutes = require('./controllers/user-controller')
 const adminRoutes = require('./controllers/admin-controller')
 const PORT = process.env.PORT ?? 8000
 
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Admin {
+    id: ID
+    username: String
+    email: String
+    password: String
+  }
+
+  type User{
+    id:ID
+    username:String
+    fullname:String
+    email:String
+    password:String
+    course:Course
+  }
+
+  type Course{
+    id:ID
+    title:String
+    description: String
+    adminId:String
+  }
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    getAllAdmin: [Admin]
+    getAllCourses: [Course]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getAllAdmin: async() => {
+      const admins = await prisma.admin.findMany()
+      return admins
+    },
+    getAllCourses: async () => {
+      const courses = await prisma.course.findMany();
+      return courses
+    }
+  },
+};
+
 const app = express();
 
 export const prisma = new PrismaClient()
 
 async function main() {
+   
+
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    });
     app.use(bodyParser.json())
     app.use(cors());
+    await server.start();
 
+  
+  app.use('/graphql',expressMiddleware(server))
   app.use('/user', userRoutes)
   app.use('/admin',adminRoutes)
     app.get('/', (req, res) => {
